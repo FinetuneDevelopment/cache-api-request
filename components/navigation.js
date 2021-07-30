6e66e6c392921ae01bac855de835a25f5b93910c
Ross Angus <ross.a.angus@gmail.com>
2021-07-30 12:16:03 +0100

    Added environmental variables to the navigation component

--- components/navigation.js
+++ components/navigation.js
@@ -2,12 +2,16 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
+const currentURL = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  : "http://localhost:3000";
+
 export default function Navigation() {
   const [pageList, setPageList] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("http://localhost:3000/api/get-page-list");
+        const response = await fetch(`http://${currentURL}/api/get-page-list`);
         const json = await response.json();
         setPageList(json);
       } catch (error) {
